4828e092f1ae8e43ad8469dd606d76e70c9a8ad6
Muhammad Sufian Asad <muhammadsufian8888@gmai.com>
2026-02-08 17:19:36 +0500

    feat(orders): Register PurchaseOrderViewSet in the router

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import DeliveryViewSet, OrderListCreateView, OrderDetailView
+from .views import DeliveryViewSet, OrderListCreateView, OrderDetailView, PurchaseOrderViewSet
 
 router = DefaultRouter()
 router.register(r'deliveries', DeliveryViewSet, basename='delivery')
+router.register(r'purchase-orders', PurchaseOrderViewSet, basename='orders-purchase-order')
 
 urlpatterns = [
     path('', OrderListCreateView.as_view(), name='orders-list'),
